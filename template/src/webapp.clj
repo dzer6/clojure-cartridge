4e17bf98c1b81b695c6e947a65eabe592bfa64b3
Shanmu Thiagaraja <shanmuha@gmail.com>
2015-05-31 10:36:46 +0100

    corrected aleph startup for openshift ip

--- template/src/webapp.clj
+++ template/src/webapp.clj
@@ -8,5 +8,6 @@
 
 (defn -main [& args]
   (let [port (Integer/parseInt (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_PORT" "8080"))
-        ip (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_IP" "0.0.0.0")]
-    (http/start-server app {:ip ip :port port})))
+        ip (get (System/getenv) "OPENSHIFT_CLOJURE_HTTP_IP" "0.0.0.0")
+		socket-address (java.net.InetSocketAddress. (java.net.InetAddress/getByName ip) port) ]
+    (http/start-server app {:socket-address socket-address})))
